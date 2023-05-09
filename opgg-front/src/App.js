c2c5854d91424d35cb201f8d24bcc7f655f72dab
akm4545 <akm4545@naver.com>
2023-05-09 20:50:25 +0900

    예제코드 register 작성 중

--- opgg-front/src/App.js
+++ opgg-front/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import { Routes } from '../node_modules/react-router-dom/dist/index';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/register" element={<RegisterPage/>}></Route>
       </Routes>
     </>
   )
--- opgg-front/src/modules/auth.js
+++ opgg-front/src/modules/auth.js
@@ -1,8 +1,14 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
+import createRequestSaga, { createRequestActionTypes } from "../lib/createRequestSage";
+import {takeLatest} from 'redux-sage/effects';
+import * as authAPI from '../lib/api/auth';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes('auth/REGISTER');
+const [LOGIN, LOGIN_SUCCEESS, LOGIN_FAILURE] = createRequestActionTypes('auth/LOGIN');
 
 export const changeField = createAction(
     CHANGE_FIELD,
@@ -14,6 +20,22 @@ export const changeField = createAction(
 );
 
 export const initializeForm = createAction(INITIALIZE_FORM, form => form);
+export const register = createAction(REGISTER, ({username, password}) => ({
+    username,
+    password,
+}));
+export const login = createAction(LOGIN, ({username, password}) => ({
+    username,
+    password,
+}));
+
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestActionTypes(LOGIN, authAPI.login);
+
+export function* authSaga(){
+    yield takeLatest(REGISTER, registerSaga);
+    yield takeLatest(LOGIN, loginSaga);
+}
 
 const initialState = {
     register: {
@@ -25,6 +47,8 @@ const initialState = {
         username: '',
         password: ''
     },
+    auth: null,
+    authError: null,
 };
 
 const auth = handleActions(
@@ -36,7 +60,26 @@ const auth = handleActions(
         ),
         [INITIALIZE_FORM]: (state, {payload: form}) => ({
            ...state,
-           [form]: initialState[form], 
+           [form]: initialState[form],
+           authError: null, 
+        }),
+        [REGISTER_SUCCESS]: (state, {payload: auth}) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        [REGISTER_FAILURE]: (state, {payload: error}) => ({
+            ...state,
+            authError: error,
+        }),
+        [LOGIN_SUCCEESS]: (state, {payload: auth}) => ({
+            ...state,
+            authError: null,
+            auth,
+        }),
+        [LOGIN_FAILURE]: (state, {payload: error}) => ({
+            ...state,
+            authError: error,
         }),
     },
     initialState
