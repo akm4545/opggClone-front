77cd1c6094bfcc07bc218688ea0e65bc954e6a4a
akm4545 <akm4545@naver.com>
2023-05-11 20:16:43 +0900

    containers 주석 작성 중

--- opgg-front/src/containers/auth/LoginForm.js
+++ opgg-front/src/containers/auth/LoginForm.js
@@ -8,8 +8,14 @@ import {check} from '../../modules/user';
 //containers = 리덕스와 연동되어 상태를 받거나 액션을 디스패치 하는 컴포넌트
 
 const LoginForm = ({history}) => {
+    //useState = state값 관리 useState(기본값)으로 사용
+    //setError = state값을 변경하는 함수
     const [error, setError] = useState(null);
+
+    //useDispatch() = 액션 생성 함수 반환
     const dispatch = useDispatch();
+
+    //useSelector() = 루트 리듀서에 등록된 값을 받아온다
     const {form, auth, authError, user} = useSelector(({auth, user}) => ({
         form: auth.login,
         auth: auth.auth,
