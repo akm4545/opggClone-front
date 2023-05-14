5dec023e8439669fad2d4af92ae3bbbd7f084085
akm4545 <akm4545@naver.com>
2023-05-14 21:19:59 +0900

    임시 인덱스 페이지 생성 (링크 달아서 사용

--- opgg-front/src/App.js
+++ opgg-front/src/App.js
@@ -3,11 +3,13 @@ import {Route} from 'react-router-dom';
 import { Routes } from '../node_modules/react-router-dom/dist/index';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import TempIndex from './pages/TempIndex';
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<TempIndex/>}></Route>
         <Route path="/login" element={<LoginPage/>}></Route>
         <Route path="/register" element={<RegisterPage/>}></Route>
       </Routes>
---
+++ opgg-front/src/pages/TempIndex.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//index 페이지 생성 전 임시 페이지
+const TempIndex = () => {
+    return (
+        <>
+            <div>
+                <ul>
+                    <li>
+                        임시1 링크
+                    </li> 
+                    <li>
+                        임시1 링크
+                    </li>
+                </ul> 
+            </div>
+        </>
+    );
+};
+
+export default TempIndex;
