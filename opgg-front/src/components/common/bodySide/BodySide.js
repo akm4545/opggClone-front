14e27261e05b68c22f8027aa3d848bcd2257c0c4
akm4545 <akm4545@naver.com>
2023-05-16 20:11:52 +0900

    side 광고 컴포넌트 작성 중

---
+++ opgg-front/src/components/common/bodySide/BodySide.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const BodySide = () => {
+    return <>
+        <div class="css-85l9ui e17e77tq10">
+            <div class=" css-1no9azl e17e77tq9">
+                <div id="div-gpt-ad-1675834072960-0" style={{minWidth: "120px", minHeight: "600px;"}} data-google-query-id="CIOk9ITS-f4CFThAwgUduskOmg">
+                    <div id="" style={{border: ["0pt", "none"], display: "inline-block", width: "300px", height: "600px"}}>
+                        <image></image>
+                        <iframe frameborder="0"
+                            src="https://opgg-static.akamaized.net/logo/20230504114546.931140e7d4574a3ba63ae5783b4faa26.png?image=q_auto,f_webp,w_auto&amp;v=1684072072534"
+                            id="" title="3rd party ad content"
+                            name="" scrolling="no" marginwidth="0" marginheight="0" width="300" height="600"
+                            data-is-safeframe="true"
+                            sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+                            role="region" aria-label="Advertisement" tabindex="0" data-google-container-id="2"
+                            style={{border: "0px", verticalAlign: "bottom"}} data-load-complete="true">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+            <div class=" css-1no9azl e17e77tq9">
+                <div id="div-gpt-ad-1675834102092-0" style={{minWidth: "120px", minHeight: "600px"}} data-google-query-id="CMa7hYXS-f4CFZMOXAodrssHag">
+                    <div id="" style={{border: ["0pt", "none"]}}>
+                        <iframe id=""
+                            src="https://opgg-static.akamaized.net/logo/20230504114546.931140e7d4574a3ba63ae5783b4faa26.png?image=q_auto,f_webp,w_auto&amp;v=1684072072534"
+                            name="google_ads_iframe_/114726905/opgg_leaderboards_pc_right_300x600_0"
+                            title="3rd party ad content" width="300" height="600" scrolling="no" marginwidth="0"
+                            marginheight="0" frameborder="0" role="region" aria-label="Advertisement" tabindex="0"
+                            style={{border: "0px", verticalAlign: "bottom"}} data-load-complete="true"
+                            data-google-container-id="3">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+};
+
+export default BodySide;
--- opgg-front/src/pages/LeaderboardPage.js
+++ opgg-front/src/pages/LeaderboardPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import OpggHeader from "../components/common/header/OpggHeader";
 import OpggFooter from "../components/common/footer/OpggFooter";
 import BodyHeaderMenu from "../components/common/bodyHeaderMenu/BodyHeaderMenu";
+import BodySide from "../components/common/bodySide/BodySide";
 
 const LeaderboardPage = () => {
     return <>
             <OpggHeader></OpggHeader>
             <BodyHeaderMenu></BodyHeaderMenu>
+            <BodySide></BodySide>
             <OpggFooter></OpggFooter>
         </>
 }
