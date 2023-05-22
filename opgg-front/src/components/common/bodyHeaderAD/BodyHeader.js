af80495159736eeb819bb2ce6f35965067f847c1
akm4545 <akm4545@naver.com>
2023-05-22 19:57:46 +0900

    메인 상단 광고 컴포넌트 작성

---
+++ opgg-front/src/components/common/bodyHeaderAD/BodyHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BodyHeader = () => {
+    return <>
+        <div id="banner-container" className="css-1ee8xg9 e17e77tq11">
+            <div id="div-gpt-ad-1675834170356-0" style={{minWidth: "728px", minHeight: "90px"}} data-google-query-id="COKxpdbeiP8CFZMCXAodqUgPpg">
+                <div id="google_ads_iframe_/114726905/opgg_leaderboards_pc_top_970x90_0__container__" style={{border: "0pt none"}}>
+                    <iframe id="google_ads_iframe_/114726905/opgg_leaderboards_pc_top_970x90_0"
+                        src="https://opgg-static.akamaized.net/logo/20230504114546.931140e7d4574a3ba63ae5783b4faa26.png?image=q_auto,f_webp,w_auto&amp;v=1684072072534"
+                        name="google_ads_iframe_/114726905/opgg_leaderboards_pc_top_970x90_0" title="3rd party ad content"
+                        width="970" height="90" scrolling="no" marginWidth="0" marginHeight="0" frameBorder="0" role="region"
+                        aria-label="Advertisement" tabIndex="0" style={{border: "0px", verticalAlign: "bottom"}} 
+                        data-load-complete="true" data-google-container-id="3"></iframe></div>
+            </div>
+        </div>
+    </>;
+};
+
+export default BodyHeader;
--- opgg-front/src/pages/LeaderboardPage.js
+++ opgg-front/src/pages/LeaderboardPage.js
@@ -2,13 +2,15 @@ import React from "react";
 import OpggHeader from "../components/common/header/OpggHeader";
 import OpggFooter from "../components/common/footer/OpggFooter";
 import BodyHeaderMenu from "../components/common/bodyHeaderMenu/BodyHeaderMenu";
-import BodySide from "../components/common/bodySide/BodySide";
+import BodySide from "../components/common/bodySideAD/BodySide";
+import BodyHeader from "../components/common/bodyHeaderAD/BodyHeader";
 
 const LeaderboardPage = () => {
     return <>
             <OpggHeader></OpggHeader>
             <BodyHeaderMenu></BodyHeaderMenu>
             <BodySide></BodySide>
+            <BodyHeader></BodyHeader>
             <OpggFooter></OpggFooter>
         </>
 }
