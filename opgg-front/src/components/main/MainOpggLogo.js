292022cf9e410055e9c9a22f7270d1f6d9f89ca4
leesungkyu1 <lccab2002@gmail.com>
2023-06-19 15:50:49 +0900

    main화면 css작업 완료

--- opgg-front/src/components/main/MainOpggLogo.js
+++ opgg-front/src/components/main/MainOpggLogo.js
@@ -4,9 +4,8 @@ const MainOpggLogo = () => {
     return<>
             <div className="css-i7pvjw e1sjm9eu0">
                 <img
-                    src="https://opgg-static.akamaized.net/logo/20230526145903.
-            0507b4bb36bf4c5684ad6eb87761b9e1.png?image=q_auto,f_webp,w_auto&amp;v=1685352893390"
-                    alt="OP.GG logo (니코 리워크)" title="니코 리워크"/>
+                    src="https://opgg-static.akamaized.net/logo/20230601050340.43a02ea2187741c6af65599282dba854.png?image=q_auto,f_webp,w_auto&v=1686926528598"
+                    alt="" title="니코 리워크"/>
             </div>
         </>
 };
