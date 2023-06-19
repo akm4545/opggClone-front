4213543965f954ae17a84ff25c29a6f1619273dd
akm4545 <akm4545@naver.com>
2023-06-19 19:19:41 +0900

    에러 페이지 작업 중:

--- opgg-front/src/components/leaderboard/LeaderBoardWrapper.js
+++ opgg-front/src/components/leaderboard/LeaderBoardWrapper.js
@@ -2,25 +2,31 @@ import React from "react";
 import LeaderBoardNotice from "./LeaderBoardNotice";
 import LeaderBoardSearch from "./LeaderBoardSearch";
 import LeaderBoardTable from "./LeaderBoardTable";
+import Error from "../common/error/Error";
 
 const LeaderBoardWrapper = ({leaderboardList, loading, err, userName, percentageOfvictoriesCalc, page, onChangeValue, onKeyUp, onClick}) => {
     return <>
         <div id="content-container" className="css-wt4c1a e2v0byd0">
-            <LeaderBoardNotice></LeaderBoardNotice>
-            <LeaderBoardSearch
-                onChangeValue={onChangeValue}
-                onKeyUp={onKeyUp}
-                userName={userName}    
-            />
-            <LeaderBoardTable
-                leaderboardList={leaderboardList}
-                loading={loading}
-                err={err}
-                page={page}
-                userName={userName}
-                percentageOfvictoriesCalc={percentageOfvictoriesCalc}
-                onClick={onClick}
-            />
+            {leaderboardList === true ? (<>
+                <LeaderBoardNotice></LeaderBoardNotice>
+                <LeaderBoardSearch
+                    onChangeValue={onChangeValue}
+                    onKeyUp={onKeyUp}
+                    userName={userName}    
+                />
+                <LeaderBoardTable
+                    leaderboardList={leaderboardList}
+                    loading={loading}
+                    err={err}
+                    page={page}
+                    userName={userName}
+                    percentageOfvictoriesCalc={percentageOfvictoriesCalc}
+                    onClick={onClick}
+                />
+            </>
+            ) : (<>
+                <Error></Error>
+            </>)}
         </div>
     </>
 };
