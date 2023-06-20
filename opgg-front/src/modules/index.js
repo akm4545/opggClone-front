fbd294f46366087b33954c138d6f3390e7798f97
leesungkyu1 <lccab2002@gmail.com>
2023-06-20 17:05:03 +0900

    main 검색기능 완료

--- opgg-front/src/modules/index.js
+++ opgg-front/src/modules/index.js
@@ -4,17 +4,19 @@ import auth, {authSaga} from './auth';
 import loading from "./loading";
 import user, {userSaga} from './user';
 import leaderboard, {leaderboardSaga} from "./leaderboard";
+import summoner, {summonerSaga} from "./main";
 
 //루트 사가에 등록
 const rootReducer = combineReducers({
     auth,
     loading,
     user,
-    leaderboard
+    leaderboard,
+    summoner
 });
 
 export function* rootSaga(){
-    yield all([authSaga(), userSaga(), leaderboardSaga()]);
+    yield all([authSaga(), userSaga(), leaderboardSaga(), summonerSaga()]);
 };
 
 export default rootReducer;
