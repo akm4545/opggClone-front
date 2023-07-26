609c101fdf789258b847afff7783c756dd626c0b
akm4545 <akm4545@naver.com>
2023-07-26 12:10:02 +0900

    통신 테스트

--- opgg-front/src/lib/api/main.js
+++ opgg-front/src/lib/api/main.js
@@ -1,6 +1,7 @@
 import asyncRequestBase from "./asyncRequestBase";
 
-const baseUrl = "http://127.0.0.1:8000/"
+//const baseUrl = "http://127.0.0.1:8000/"
+const baseUrl = "http://webapp:8000/"
 
 export const AdList = () => {
     return asyncRequestBase("GET", `${baseUrl}ads`);
