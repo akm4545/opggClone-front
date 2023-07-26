961b1b7a46c7388f54a7cc0f58bbe62349c7ced2
akm4545 <akm4545@naver.com>
2023-07-26 15:05:20 +0900

    테스트 서버 url 변경

--- opgg-front/src/lib/api/leaderBoard.js
+++ opgg-front/src/lib/api/leaderBoard.js
@@ -1,7 +1,7 @@
 import asyncRequestBase from "./asyncRequestBase";
 
 //const baseUrl = "http://127.0.0.1:8000/"
-const baseUrl = "http://webapp:8000/"
+const baseUrl = "http://101.101.216.55:8000/"
 
 export const leaderBoardList = ({page}) => {
     return asyncRequestBase("GET", `${baseUrl}leaderBoard?page=${page}`);
